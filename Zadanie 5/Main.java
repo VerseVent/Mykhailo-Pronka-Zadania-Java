a6784c05c2763de76b2e1c2d9ca10cfd056dba95
VerseVent <60715161+VerseVent@users.noreply.github.com>
2020-02-05 22:26:53 +0100

    Update Main.java

--- Zadanie 5/Main.java
+++ Zadanie 5/Main.java
@@ -4,46 +4,18 @@ import java.util.Random;
 
 public class Main{
     public static void main (String[] args){
-        int [] array = new int[6];
-        Random rand = new Random();
-        for (int i=0;i<array.length; i++){
-            array [i] = rand.nextInt(50);
-            for (int k=0; k<i; k++){
-                if (array [k] == array[i]){
-                    array[i]=rand.nextInt(50);
+        int [] arr = new int[6];
+        Random r = new Random();
+        for (int i=0;i<arr.length; i++){
+            arr [i] = r.nextInt(50);
+            for (int l=0; l<i; l++){
+                if (arr [l] == arr[i]){
+                    arr[i]=r.nextInt(50);
 
                 }
             }
         }
-        System.out.println(Arrays.toString(array));
+        System.out.println(Arrays.toString(arr));
 
     }
 }
-/*
- *
- *
- *
- */
-//// losuje 6 roznych z 49 liczb (uzywajac kolekcji)
-//package com.company;
-//        import java.util.*;
-//public class Main {
-//    public static void main(String[] args) {
-//        int liczba;
-//        Set<Integer> set = new HashSet<>();
-//        Random rand = new Random();
-//
-//        for ( int i = 0; i < 6; i++) {
-//            liczba = rand.nextInt(50);
-//            set.add(liczba);
-//        }
-//        if (set.size()==6){
-//            System.out.println(set);
-//        }else {
-//            int liczba_2;
-//            liczba_2 =  rand.nextInt(50);
-//            set.add(liczba_2);
-//            System.out.println(set);
-//        }
-//    }
-//}
